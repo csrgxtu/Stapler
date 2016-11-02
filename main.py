3918dfc3b080ee50cddcd539825f95ca0aa30e19
Archer Reilly <1246506786@qq.com>
2016-11-02 10:23:40 +0800

    make it py

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding=utf-8
+#
+# Author: Archer
+# File: main.py
+# Desc: make this project Python
+# Date: 02/Nov/2016
+#
+# Produced By CSRGXTU
+
+print 'Stapler is a repo contains solutions to some quizzes'
